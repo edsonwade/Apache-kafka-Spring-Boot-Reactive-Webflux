7665fc213bd44c0347a930bccda113dc1e70675f
edsonwade <vanilsonmuhongo@outlook.pt>
2024-02-14 00:49:33 +0000

    Initial commit

---
+++ src/main/java/code/vanilson/apachekafka/ApacheKafkaApplication.java
@@ -0,0 +1,13 @@
+package code.vanilson.apachekafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheKafkaSpringBootReactiveWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApacheKafkaSpringBootReactiveWebfluxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/code/vanilson/apachekafka/ApacheKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package code.vanilson.apachekafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheKafkaSpringBootReactiveWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
