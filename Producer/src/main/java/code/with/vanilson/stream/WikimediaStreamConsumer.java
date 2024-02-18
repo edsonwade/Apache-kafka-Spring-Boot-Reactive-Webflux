7969cfb67820da25dfc7f71d32de06815796700f
edsonwade <vanilsonmuhongo@gamil.com>
2024-02-18 16:38:34 +0000

    publish our MediaWiki from producer

--- Producer/src/main/java/code/with/vanilson/stream/WikimediaStreamConsumer.java
+++ Producer/src/main/java/code/with/vanilson/stream/WikimediaStreamConsumer.java
@@ -2,7 +2,6 @@ package code.with.vanilson.stream;
 
 import code.with.vanilson.ProducerWikimedia;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
